c9fbc65110c97b653a8b67da3cb5b6774e322442
TALEX1995 <tanase.alex@libero.it>
2023-06-01 16:58:31 +0200

    Added img into DOM

--- script/script.js
+++ script/script.js
@@ -1,1 +1,29 @@
 console.log('JS OK');
+
+// Recover Element from DOM
+
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+const gallery = document.querySelector('.gallery');
+
+
+// Prepare IMG Array
+
+const imgArray = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp',];
+
+// Prepare Element to insert into DOM gallery
+
+let imageElements = '';
+
+
+// Prepare Cicle to insert the element into imageElements
+
+for (let i = 0; i < imgArray.length; i++) {
+
+    imageElements += `<img src="${imgArray[i]}">`;
+}
+
+// Insert img into DOM
+
+gallery.innerHTML = imageElements
